3e4cb71ccb7020506ffe7dd911092ab686146087
11as <11as@A0580.alewijnse.local>
2017-12-21 00:29:49 +0100

    Day 5 - puzzle 1 finished

---
+++ puzzles/day5-puzzle1.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	ifstream inputFile;
+	//inputFile.open("../input/day5_puzzle1_example_input.txt");
+	inputFile.open("../input/day5_puzzle1_input.txt");
+	string line;
+
+	if (!inputFile.is_open()) {
+		cout << "Unable to open file" << endl;
+		return -1;
+	}
+
+	vector<int> il;
+	il.clear();
+
+	while ( getline (inputFile,line) ) {
+		int number = atoi(line.c_str());
+		il.push_back(number);
+	}
+	inputFile.close();
+
+	/*for (int i = 0; i < il.size(); i++) {
+		cout << il.at(i) << endl;
+	}
+	cout << il.size() << endl;*/
+
+	int index = 0;
+	int newIndex;
+	int steps = 0;
+
+	while (index < il.size()) {
+		newIndex = index + il.at(index);
+		il.at(index)++;
+		index = newIndex;
+		steps++;
+	}
+	cout << steps << endl;
+	return 0;
+}
